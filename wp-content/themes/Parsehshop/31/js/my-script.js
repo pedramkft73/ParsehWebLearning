a04b2db40d6cbc2dfdbea28435de916ff022e1a1
Pedram <pedram.pourzeynali@gmail>
2021-12-10 23:23:30 +0330

    'jalaseye-2-afzudane-fileha'

---
+++ wp-content/themes/Parsehshop/31/js/my-script.js
@@ -0,0 +1,78 @@
+		$(document).ready(function(){
+			
+			$('.one-time').slick({
+				arrows: false,
+				rtl: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+//				dots: true,
+				autoplay: true,
+				autoplaySpeed: 3000,	
+			  });
+			
+			$('.last-products-slider').slick({
+				arrows: true,
+				rtl: true,
+				slidesToShow: 4,
+				slidesToScroll: 1,
+//				dots: true,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				  responsive: [
+					{
+					  breakpoint: 992,
+					  settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: false
+					  }
+					},
+					{
+					  breakpoint: 576,
+					  settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					  }
+					},
+					{
+					  breakpoint: 480,
+					  settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					  }
+					}
+					// You can unslick at a given breakpoint now by adding:
+					// settings: "unslick"
+					// instead of a settings object
+				  ]
+			  });
+			
+			$('.last-blog-slider').slick({
+				arrows: false,
+				rtl: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+				autoplay: true,
+				autoplaySpeed: 3000,	
+			  });
+			
+			
+			
+			$('.myplus').click(function(){
+						var myval = Number($(this).parent('.pm-icon').find('input').val());
+							myval=myval+1;
+							$(this).parent('.pm-icon').find('input').val(myval);					   
+							   
+							   });
+			$('.myminus').click(function(){
+						var myval = Number($(this).parent('.pm-icon').find('input').val());
+							myval=myval-1;
+							$(this).parent('.pm-icon').find('input').val(myval);					   
+							   
+							   });			
+			
+			
+		});	
+		
